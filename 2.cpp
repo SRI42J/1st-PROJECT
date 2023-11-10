febc44d47c945ebaf6636dfded061be47b38c601
S2a3m <150414585+S2a3m@users.noreply.github.com>
2023-11-10 14:51:16 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
 	int  i,n;
-	cout<<"enter n ";
+	cout<<"enter n value: ";
 	cin>>n;
 	i=1;
-	while(1<=n)
+	while(i<=n)
     {
-    	cout<<i
+    	cout<<i;
     	i++;
 	}
 }
